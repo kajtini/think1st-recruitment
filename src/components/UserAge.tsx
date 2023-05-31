0f78cbff0d81ed5df917d526cc8a1d32ea01f385
Kajetan Kowalski <kowalskikajetan824@gmail.com>
2023-05-31 15:00:34 +0200

    fix spacing

--- src/components/UserAge.tsx
+++ src/components/UserAge.tsx
@@ -23,28 +23,30 @@ const UserAge = ({ handleChange, age }: UserAgeProps) => {
     return (
         <label className="flex flex-col gap-2 relative mb-6">
             Age
-            <div className="flex items-center justify-between">
-                <span className="text-center text-xs">0</span>
-                <span className="text-center text-xs">100</span>
-            </div>
-            <div className="relative">
-                <input
-                    ref={ageRef}
-                    name="age"
-                    className="appearance-none rounded-full h-1 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-very-dark-purple cursor-pointer w-full"
-                    id="age"
-                    type="range"
-                    min="0"
-                    max="100"
-                    required
-                    value={age}
-                    onChange={handleChange}
-                />
-                <div
-                    ref={ageTooltipRef}
-                    className="absolute -translate-x-1/2 mt-1  w-9 h-6 border-very-dark-purple border bg-very-light-gray rounded-md flex items-center justify-center text-xs text-very-dark-purple"
-                >
-                    <span>{age}</span>
+            <div>
+                <div className="flex items-center justify-between">
+                    <span className="text-center text-xs">0</span>
+                    <span className="text-center text-xs">100</span>
+                </div>
+                <div className="relative">
+                    <input
+                        ref={ageRef}
+                        name="age"
+                        className="appearance-none rounded-full h-1 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-very-dark-purple cursor-pointer w-full"
+                        id="age"
+                        type="range"
+                        min="0"
+                        max="100"
+                        required
+                        value={age}
+                        onChange={handleChange}
+                    />
+                    <div
+                        ref={ageTooltipRef}
+                        className="absolute -translate-x-1/2 mt-1  w-9 h-6 border-very-dark-purple border bg-very-light-gray rounded-md flex items-center justify-center text-xs text-very-dark-purple"
+                    >
+                        <span>{age}</span>
+                    </div>
                 </div>
             </div>
         </label>
